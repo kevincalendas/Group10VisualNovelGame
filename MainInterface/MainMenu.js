9c2f8d13be4845582e271c7b0ed645a1434865b8
kevincalendas <johnkevixcalendas@gmail.com>
2026-02-07 17:44:20 +0800

    Improved Animations

--- MainInterface/MainMenu.js
+++ MainInterface/MainMenu.js
@@ -14,31 +14,34 @@ overlaything.addEventListener('click', () => {
         overlaything.style.display = 'none';
         
         setTimeout(() => {
-            MainIntro1H.style.animation = 'MainIntroPanel 0.1s ease 1s 4 forwards, MainIntroPanel1 0.1s ease 2.5s 2 forwards';
+            MainIntro1H.style.animation = 'MainIntroPanel 0.08s ease 1.05s 4 forwards, MainIntroPanel1 0.07s ease 2.5s 3 forwards';
             setTimeout(() => {
                 MainMenuSFX.play();
-            }, 700);
+            }, 850);
             
             setTimeout(() => {
                 MainMenuSFX.pause();
-            }, 900);
+            }, 1300);
 
             setTimeout(() => {
                 MainMenuSFX.play();
-            }, 2170);
+            }, 2300);
 
             setTimeout(() => {
                 MainMenuSFX.pause();
-            }, 2250);
+            }, 2500);
 
 
             // closing the static sound effect and starting the music at the same time to create a smooth transition //
             setTimeout(() => {
                 MainMenuMusicBackground.play();
             }, 4700);
-            MainIntroPanel.style.animation = 'Fadeout 1s ease 4s forwards';
-            MainIntroPanel1.style.animation = 'Fadein 1s ease 4.5s forwards';
-        }, 0);
+
+            // ScaleDownAnimation // scale down Animation for the Main Menu //
+            MainIntroPanel.style.animation = 'Fadeout 0.5s ease 4s both, ScaleDownAnimation 1s ease 4s both, MainTextShake3 0.5s ease 2.5s 1 both';
+            MainIntroPanel1.style.animation = 'Fadein 1s ease 4.5s forwards, ScaleDownAnimation1 1s ease 4.2s forwards';
+            
+        }, 0);  
     }, 1000);
 });
 
