c966826e6a24aa0253808c8089392be7afe275fe
kevincalendas <johnkevixcalendas@gmail.com>
2026-02-08 15:51:33 +0800

    test

--- MainInterface/MainMenu.js
+++ MainInterface/MainMenu.js
@@ -59,19 +59,81 @@ overlaything.addEventListener('click', () => {
 
 // Main Menu Button1 Open Function with Animation //
 
+
+// PLAY STORY SYSTEMS. //
+
 function PlayButtonDirect() {
+    const LoadingScreenDIV = document.getElementById("LoadingScreenDIV");
+    const HeadphoneSug = document.getElementById("HeadphoneSug");
+    const LoadingScreenText = document.getElementById("LoadingScreenText");
+    const LoadingScreenText1 = document.getElementById("LoadingScreenText1");
+    const MainDialoguePanel = document.getElementById("MainDialoguePanel");
+    const elem = document.documentElement;
+    var MainMenuSFX = document.getElementById("MainMenuSFX1");
+    var OpeningIntro1 = document.getElementById("OpeningIntro1");
     setTimeout(() => {
         MainIntroPanel1.style.animation = 'Fadeout 0.5s ease forwards, ScaleUPAnimation 1s ease 0.1s forwards';
         setTimeout(() => {
+            MainIntroPanel1.style.display = 'none';
             LoadingText.style.animation = 'Fadein 1s ease 0.5s forwards';
             LoadingText.style.display = 'block';
             setTimeout(() => {
                 LoadingText.style.animation = 'Fadeout 1s ease 0.5s forwards';
                 setTimeout(() => {
                     LoadingText.style.display = 'none';
-                    window.location.href = "../StoryInterface/StoryInterface.html";
+                    ShowLoadingScreen();
                 }, 1000);
             }, 5000);
         }, 1000);
     }, 500);
+}
+
+function ShowLoadingScreen() {
+    LoadingScreenDIV.style.display = "flex";
+    MainMenuMusicBackground.pause();
+    setTimeout(() => {
+        LoadingScreenDIV.style.animation = "Fadein 1s ease both";
+        LoadingScreenText.style.animation = "MainIntroPanel1 0.07s ease 2.5s 3 forwards, MainIntroPanel 0.08s ease 2.5s 4 forwards, MainTextShake3 0.5s ease 2.5s 1 both, Fadeout 0.5s ease 5s both";
+        setTimeout(() => {
+            MainMenuSFX.play();
+        }, 2300);
+            
+        setTimeout(() => {
+            MainMenuSFX.pause();
+        }, 2600);
+        
+        setTimeout(() => {
+            OpeningIntro1.play();
+        }, 5300);
+        HeadphoneSug.style.animation = "Fadein 0.5s ease 5.5s both";
+        HeadphoneSug.style.transform = "translateY(25px)";
+        HeadphoneSug.style.animation = "MainmoveHeadphone1 0.5s ease 6.3s forwards, Fadein 0.5s ease 5.5s forwards, Fadeout 2s ease 10.5s forwards";
+        LoadingScreenText1.style.animation = "Fadein 0.5s ease 6.3s forwards, Fadeout 2s ease 10.5s forwards";
+        setTimeout(() => {
+            Line1AStory()
+        }, 12500);
+    }, 5000);
+}   
+
+const MainDialogueCenterPanel = document.getElementById("MainDialogueCenterPanel");
+const MainDialogueCenterText = document.getElementById("MainDialogueCenterText");
+
+let CurrentPreviousLine = 0;
+let currentStoryLine = 0;
+function Line1AStory() {
+    console.log("Showing first line...");
+    MainDialogueCenterPanel.style.display = "flex";
+    MainDialogueCenterText.innerHTML = '<span class="SentenceFade">In this story, there are four friends—Kevin, AJ, Lyza, and Rhema. </span>';
+    setTimeout(() => {
+        console.log("Showing second line...");
+        MainDialogueCenterText.insertAdjacentHTML('beforeend', '<span class="SentenceFade">One day, they decided to have a sleepover at Rhema’s house, which was located near a dark and silent forest. </span>');
+        setTimeout(() => {
+            console.log("Showing thrid line...");
+            MainDialogueCenterText.insertAdjacentHTML('beforeend', '<span class="SentenceFade">Their friendship was strong; they treated each other like family, bound together like siblings. </span>');
+        }, 5500);
+    }, 5500);
+    
+}
+
+function Line2AStory() {
 }
