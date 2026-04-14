5ec703d2acb1e6cdb4f0d35c77606f88b981f58b
kevinCalendas <johnkevixcalendas@gmail.com>
2026-04-15 01:04:35 +0800

    ADDED FEATURES: - Polished UI Interface - Improved Animations
    BUGS:
    - Lags when opening load section and exiting load sections.

--- MainInterface/MainLoad.js
+++ MainInterface/MainLoad.js
@@ -1,10 +1,15 @@
 function OpenLoadMenu() {
     const MainIntroPanel1 = document.getElementById("MainIntroPanel1");
     const MainContainerofLoadUI = document.getElementById("MainContainerofLoadUI");
+    const LoadGameH1A1 = document.getElementById("LoadGameH1A1");
+    const LoadGameContainer = document.getElementById("LoadGameContainer");
 
-    MainIntroPanel1.style.animation = 'ScaleUPAnimation 0.5s ease-out forwards, Fadeout 0.5s ease-out forwards';
+    MainIntroPanel1.style.animation = 'Fadeout 0.5s ease both, ScaleUPAnimation 1s ease 0.1s both';
     MainContainerofLoadUI.style.display = "block";
-    MainContainerofLoadUI.style.animation = "ScaleDownAnimation1 1s ease-out forwards 1s, Fadein 1s ease-out forwards 1.3s";
+    MainContainerofLoadUI.style.animation = "ScaleDownAnimation1 1s ease-out both 1s, Fadein 1s ease-out both 1s";
+
+    LoadGameH1A1.style.animation = 'Fadein 1s ease 1.7s both, FadeInScaleAnimationIntroleft 1s ease 1.7s both';
+    LoadGameContainer.style.animation = 'Fadein 1s ease 2s both, FadeInScaleAnimationIntroleft 1s ease 2s both';
 
 }
 
@@ -13,10 +18,10 @@ function CloseLoadMenu() {
     const MainContainerofLoadUI = document.getElementById("MainContainerofLoadUI");
 
 
-    MainContainerofLoadUI.style.animation = 'ScaleUPAnimation 0.5s ease-out forwards, Fadeout 0.5s ease-out forwards';
+    MainContainerofLoadUI.style.animation = 'Fadeout 0.5s ease both, ScaleUPAnimation 1s ease 0.1s both';
         setTimeout(() => {  
             MainContainerofLoadUI.style.display = "none";
             MainIntroPanel1.style.display = "block";
-            MainIntroPanel1.style.animation = "ScaleDownAnimation1 1s ease forwards, Fadein 1s ease forwards";
+            MainIntroPanel1.style.animation = "ScaleDownAnimation1 1s ease both, Fadein 1s ease both";
         }, 700);
 }
